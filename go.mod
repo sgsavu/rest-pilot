d352b5798753c3f92ff6c8034184bc88626f5b10
Jibreel <xdarkmemoriez@gmail.com>
2024-08-22 14:07:08 +0100

    add core func

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sgsavu/restpilot
+
+go 1.22.5
---
+++ main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+type Test struct {
+	Name     string   `json:"name"`
+	Request  Request  `json:"request"`
+	Response Response `json:"response"`
+}
+
+type Request struct {
+	Method  string            `json:"method"`
+	URL     string            `json:"url"`
+	Headers map[string]string `json:"headers"`
+	Body    string            `json:"body,omitempty"`
+}
+
+type Response struct {
+	StatusCode int                    `json:"status_code"`
+	Headers    map[string]string      `json:"headers"`
+	Body       map[string]interface{} `json:"body"`
+}
+
+func loadTests(filename string) ([]Test, error) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	var tests []Test
+	err = json.Unmarshal(data, &tests)
+	if err != nil {
+		return nil, err
+	}
+
+	return tests, nil
+}
+
+func runTest(test Test) bool {
+	req, err := http.NewRequest(test.Request.Method, test.Request.URL, nil)
+	if err != nil {
+		fmt.Printf("Test %s failed: %v\n", test.Name, err)
+		return false
+	}
+
+	for key, value := range test.Request.Headers {
+		req.Header.Set(key, value)
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Printf("Test %s failed: %v\n", test.Name, err)
+		return false
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != test.Response.StatusCode {
+		fmt.Printf("Test %s failed: expected status %d, got %d\n", test.Name, test.Response.StatusCode, resp.StatusCode)
+		return false
+	}
+
+	for key, expectedValue := range test.Response.Headers {
+		actualValue := resp.Header.Get(key)
+		if actualValue != expectedValue {
+			fmt.Printf("Test %s failed: expected header %s: %s, got %s\n", test.Name, key, expectedValue, actualValue)
+			return false
+		}
+	}
+
+	fmt.Printf("Test %s passed\n", test.Name)
+	return true
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Please provide the path to the test file")
+		os.Exit(1)
+	}
+
+	testFile := os.Args[1]
+	tests, err := loadTests(testFile)
+	if err != nil {
+		fmt.Printf("Error loading tests: %v\n", err)
+		os.Exit(1)
+	}
+
+	allPassed := true
+	for _, test := range tests {
+		passed := runTest(test)
+		if !passed {
+			allPassed = false
+		}
+	}
+
+	if allPassed {
+		fmt.Println("All tests passed")
+	} else {
+		fmt.Println("Some tests failed")
+		os.Exit(1)
+	}
+}
